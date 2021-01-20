20278a0297c8c05fdc4facfd185a693cf75ccf44
apriladwiutami <62372893+apriladwiutami@users.noreply.github.com>
2021-01-20 10:27:40 +0700

    Create Akun.java

---
+++ Akun.java
@@ -0,0 +1,9 @@
+package paypay;
+
+import java.util.ArrayList;
+import javafx.beans.property.DoubleProperty;
+import javafx.beans.property.IntegerProperty;
+import javafx.beans.property.SimpleDoubleProperty;
+import javafx.beans.property.SimpleIntegerProperty;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
