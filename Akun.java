f619c60e0155a28fb356388eea8491bd4799bb30
Fan <62688472+byebyeblue@users.noreply.github.com>
2021-01-20 10:30:26 +0700

    Update Akun.java

--- Akun.java
+++ Akun.java
@@ -7,3 +7,13 @@ import javafx.beans.property.SimpleDoubleProperty;
 import javafx.beans.property.SimpleIntegerProperty;
 import javafx.beans.property.SimpleStringProperty;
 import javafx.beans.property.StringProperty;
+
+public abstract class Akun {
+    private IntegerProperty ID;
+    private StringProperty Name;
+    private StringProperty Address;
+    private StringProperty email;
+    private StringProperty no_hp;
+    private DoubleProperty saldo;
+    private ArrayList<Card> cards;
+
